b622f1dd8e86f49f4653a75094c69f05ea4623b3
jose roberto <jr.campos@hotmail.com.br>
2020-11-15 22:19:41 -0300

    projeto hellowWord

---
+++ HelloWord/src/main/java/br/com/jrcwrxg/HelloWord/HelloWorld.java
@@ -0,0 +1,19 @@
+package br.com.jrcwrxg.HelloWord;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// Informamos ao Spring que essa classe é um controller
+@Controller
+public class HelloWorld {
+	//Minha classe nova
+    // Configuramos esse método para ser executado quando
+    // o usuário acessar localhost:8080.
+    // Esse endereço é padrão.
+    @GetMapping
+    @ResponseBody
+    public String hello() {
+        return "Hello world!";
+    }
+}
---
+++ HelloWord/src/test/java/br/com/jrcwrxg/HelloWord/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jrcwrxg.HelloWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
